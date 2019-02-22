b0170f227164745ffd19612968d56a7d51ef4479
krstivoja <mailme@markokrstic.com>
2019-02-22 20:32:55 +0100

    fixed paths

--- inc/scripts.php
+++ inc/scripts.php
@@ -11,7 +11,7 @@ function twy_include_scripts_frontend(){
 			wp_enqueue_script('twy_jquery', $script_url, array(), null);
 		}
 		else{
-			wp_enqueue_script('twy_jquery', plugin_dir_url(__FILE__).'public/bower_components/jquery/dist/jquery.min.js', array(), null);
+			wp_enqueue_script('twy_jquery', plugin_dir_url(__FILE__).'../public/bower_components/jquery/dist/jquery.min.js', array(), null);
 		}
 	}
 	if(get_option('twy_swiss_knife_include_gsap')){
@@ -20,7 +20,7 @@ function twy_include_scripts_frontend(){
 			wp_enqueue_script('twy_gsap', $script_url, array(), null);
 		}
 		else{
-			wp_enqueue_script('twy_gsap', plugin_dir_url(__FILE__).'public/bower_components/gsap/src/minified/TweenMax.min.js', array(), null);
+			wp_enqueue_script('twy_gsap', plugin_dir_url(__FILE__).'../public/bower_components/gsap/src/minified/TweenMax.min.js', array(), null);
 		}
 	}
 	if(get_option('twy_swiss_knife_include_scrollmagic')){
@@ -29,7 +29,7 @@ function twy_include_scripts_frontend(){
 			wp_enqueue_script('twy_scrollmagic', $script_url, array(), null);
 		}
 		else{
-			wp_enqueue_script('twy_scrollmagic', plugin_dir_url(__FILE__).'public/bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js', array(), null);
+			wp_enqueue_script('twy_scrollmagic', plugin_dir_url(__FILE__).'../public/bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js', array(), null);
 		}
 	}
 	if(get_option('twy_swiss_knife_include_ps')){
@@ -38,7 +38,7 @@ function twy_include_scripts_frontend(){
 			wp_enqueue_script('twy_ps', $script_url, array(), null);
 		}
 		else{
-			wp_enqueue_script('twy_ps', plugin_dir_url(__FILE__).'public/bower_components/relax.js/dist/Relax.min.js', array(), null);
+			wp_enqueue_script('twy_ps', plugin_dir_url(__FILE__).'../public/bower_components/relax.js/dist/Relax.min.js', array(), null);
 		}
 	}
 	if(get_option('twy_swiss_knife_include_ss')){
@@ -47,7 +47,7 @@ function twy_include_scripts_frontend(){
 			wp_enqueue_script('twy_ss', $script_url, array(), null);
 		}
 		else{
-			wp_enqueue_script('twy_ss', plugin_dir_url(__FILE__).'public/bower_components/jquery-smoothwheel/src/jquery.smoothwheel.js', array(), null);
+			wp_enqueue_script('twy_ss', plugin_dir_url(__FILE__).'../public/bower_components/jquery-smoothwheel/src/jquery.smoothwheel.js', array(), null);
 		}
 	}
 	if(get_option('twy_swiss_knife_include_tree_js')){
@@ -56,7 +56,7 @@ function twy_include_scripts_frontend(){
 			wp_enqueue_script('twy_tree_js', $script_url, array(), null);
 		}
 		else{
-			wp_enqueue_script('twy_tree_js', plugin_dir_url(__FILE__).'public/bower_components/three.js/three.min.js', array(), null);
+			wp_enqueue_script('twy_tree_js', plugin_dir_url(__FILE__).'../public/bower_components/three.js/three.min.js', array(), null);
 		}
 	}
 }
